d07e8f6b6487769d0370bdb8397ade64f287bb2c
Sergei Ermilov <sergeiuzabila@gmail.com>
2025-08-14 23:23:22 +0300

    Tools added

---
+++ assets/js/screen-size.js
@@ -0,0 +1,18 @@
+let remove = null;
+const output = document.querySelector("#output");
+
+const updatePixelRatio = () => {
+  if (remove != null) {
+    remove();
+  }
+  const mqString = `(resolution: ${window.devicePixelRatio}dppx)`;
+  const media = matchMedia(mqString);
+  media.addEventListener("change", updatePixelRatio);
+  remove = () => {
+    media.removeEventListener("change", updatePixelRatio);
+  };
+
+  output.textContent = `${window.devicePixelRatio}`;
+};
+
+updatePixelRatio();
